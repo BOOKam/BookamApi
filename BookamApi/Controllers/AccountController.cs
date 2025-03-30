2fc221d8466ac09609b3b9cbac50af6b335e4342
engr-Godswillochi <ochigodswill@gmail.com>
2025-03-30 20:42:19 +0100

    completed user login

--- BookamApi/Controllers/AccountController.cs
+++ BookamApi/Controllers/AccountController.cs
@@ -16,10 +16,12 @@ namespace BookamApi.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<User> userManager, ITokenService tokenService)
+        private readonly SignInManager<User> _signinManager;
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signinManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -67,6 +69,38 @@ namespace BookamApi.Controllers
                     return StatusCode(500, createuser.Errors);
                 }
 
+            } catch(Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> login ([FromBody] LoginDto login)
+        {
+            try {
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var user = await _userManager.FindByNameAsync(login.Username);
+                if (user == null)
+                {
+                    return BadRequest("Invalid username or password");
+                }
+                var result = await _signinManager.CheckPasswordSignInAsync(user, login.Password, false);
+                if (!result.Succeeded)
+                {
+                    return BadRequest("Invalid username or password");
+                }
+                return Ok(
+                    new NewUserDto
+                    {
+                        Username = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
             } catch(Exception e)
             {
                 return StatusCode(500, e);
--- BookamApi/obj/Debug/net9.0/BookamApi.AssemblyInfo.cs
+++ BookamApi/obj/Debug/net9.0/BookamApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookamApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0bc50b7ad2ad57b926980422fb88fa260436d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5fde3678269609c80bc27b656f5a8331e824fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookamApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookamApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
