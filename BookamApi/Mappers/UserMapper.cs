e0fe310dd5c14e952a318163883db25b63afc3c5
engr-Godswillochi <ochigodswill@gmail.com>
2025-05-02 14:53:06 +0100

    user dto and mappers

--- BookamApi/Mappers/UserMapper.cs
+++ BookamApi/Mappers/UserMapper.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookamApi.Dtos.User;
+using BookamApi.Models;
 
 namespace BookamApi.Mappers
 {
-    public class UserMapper
+    public static class UserMapper
     {
-        
+        public static DisplayUserProfile userProfile (this User user)
+        {
+            return new DisplayUserProfile{
+                FullName = user.FullName,
+                Phone = user.Phone,
+                City = user.City,
+                ZipCode = user.ZipCode,
+                Address = user.Address,
+                State = user.State
+            };
+        }
+
+        public static User ToCreateUserProfileDto (this CreateUserProfileDto create)
+        {
+            return new User {
+                FullName = create.FullName,
+                Phone = create.Phone,
+                City = create.City,
+                ZipCode = create.ZipCode,
+                Address = create.Address,
+                State = create.State
+            };
+        }
     }
 }
